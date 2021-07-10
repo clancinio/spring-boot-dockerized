ac467fe5997fb01656207940451c7bf5f3c4a32e
Clancinio <dean.clancy91@gmail.com>
2021-07-10 15:10:58 +0100

    Init commit

---
+++ src/main/java/com/example/springbootdockerized/SpringBootDockerizedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerizedApplication.class, args);
+	}
+
+}
